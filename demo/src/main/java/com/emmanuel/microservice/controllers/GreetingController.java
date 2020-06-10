578347db401b4f07b9a814c139bb3fa2d250f786
Daniel Brandão <dcb@cin.ufpe.br>
2020-06-10 16:47:56 -0300

    Add swagger and swagger config

--- demo/src/main/java/com/emmanuel/microservice/controllers/GreetingController.java
+++ demo/src/main/java/com/emmanuel/microservice/controllers/GreetingController.java
@@ -2,6 +2,9 @@ package com.emmanuel.microservice.controllers;
 
 
 import com.emmanuel.microservice.models.Greeting;
+
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,14 +22,14 @@ public class GreetingController {
     @Value("${app.key}")
     private  String appKey;
 
-
-
+    @ApiOperation(value="Returns a greeting")
     @GetMapping()
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name)
     {
         return new Greeting(counter.incrementAndGet(), String.format(template,name));
     }
 
+    @ApiOperation(value="Return the key of the app")
     @GetMapping("/appKey")
     public Greeting getAppKey()
     {
